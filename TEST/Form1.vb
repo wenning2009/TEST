17b1ca9dcc10403f4afc0917859f99ec1eb0765c
wenning2009 <wenning2009@126.com>
2017-05-11 16:32:21 +0800

    44444444444444

--- TEST/Form1.vb
+++ TEST/Form1.vb
@@ -3,4 +3,8 @@
     Private Sub BbButton汎用1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BbButton汎用1.Click
 
     End Sub
+
+    Private Sub BbButton検索1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BbButton検索1.Click
+
+    End Sub
 End Class
