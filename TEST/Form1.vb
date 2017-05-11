5ebf46302f52bfde7994ac5d7c7230a598f53874
wenning2009 <wenning2009@126.com>
2017-05-11 16:42:59 +0800

    666

--- TEST/Form1.vb
+++ TEST/Form1.vb
@@ -7,4 +7,8 @@
     Private Sub BbButton検索1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BbButton検索1.Click
 
     End Sub
+
+    Private Sub BbButton検索2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BbButton検索2.Click
+        Dim s As String = "1"
+    End Sub
 End Class
